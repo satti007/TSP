4a714fd81e2eb08108347c01f81b42d0f7462acf
Abinash Patra <abinashpatra@Abinashs-MacBook-Air.local>
2019-02-14 19:05:57 +0530

    Mutation and Crossover added

--- src/ga.py
+++ src/ga.py
@@ -2,11 +2,22 @@ from utils import *
 from sys import stdout
 import numpy as np
 
-def mutation(childPopulation, mutateProb):
+def mutation(childPopulation, mutateProb, popSize):
 #returns children after mutation of dim popSize x numPoints
+	for i in range(popSize):
+		if np.random.random() < mutateProb:
+			childPopulation[i] = mutationUtil(childPopulation[i], mutateProb)
 
-def crossOver(selPopulation):
+	return childPopulation
+
+def crossOver(selPopulation, popSize):
 #returns children after cross-over of dim popSize x numPoints
+	selPopulation1, selPopulation2 = selPopulation[:popSize/2], selPopulation[popSize/2:]
+	childPopulation = [None]*popSize
+	for x in xrange(popSize/2):
+		childPopulation[x], childPopulation[x+(popSize/2)] = crossOverUtil(selPopulation1[x],selPopulation2[x])
+	
+	return childPopulation
 
 def selection(population,numPoints,pointDist,popSize,eliteSize):
 #returns a selected population of dim popSize x numPoints
@@ -20,8 +31,8 @@ def geneticAlgo(numPoints,pointDist,popSize,eliteSize,mutateProb,generations):
 	while True:
 		for gen in range(0,generations):
 			pop = selection(pop,numPoints,pointDist,popSize,eliteSize)
-			pop = crossOver(pop)
-			pop = mutation(pop, mutateProb)
+			pop = crossOver(pop, popSize)
+			pop = mutation(pop, mutateProb, popSize)
 			stdout.flush()
 			if gen == 10:
 				break
--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,44 @@
+import numpy as np
+
 #define your helper functions
+
+def crossOverUtil(selTour1, selTour2):
+	#returns children of dim numPoints
+	indices = np.random.randint(high=len(selTour1), size=2)
+	low, high = min(indices), max(indices)
+	gene1 = selTour1[low:high+1]
+	child1 = [None]*len(selTour1)
+	child1[low:high+1] = gene1
+	for i,x in enumerate(selTour2):
+		if i<low or i>high:
+			if x not in gene1:
+				child1[i] = x
+		else:
+			continue
+
+	indices = np.random.randint(high=len(selTour1), size=2)
+	low, high = min(indices), max(indices)
+	gene2 = selTour2[low:high+1]
+	child2 = [None]*len(selTour2)
+	child2[low:high+1] = gene2
+
+	for i,x in enumerate(selTour1):
+		if i<low or i>high:
+			if x not in gene2:
+				child2[i] = x
+		else:
+			continue
+
+	return child1, child2
+
+def mutateUtil(tour,mutateProb):
+	for i in range(len(tour)):
+		if np.random.random() < mutateProb/2:
+			ind = np.random.randint(high=len(tour))
+			temp = tour[i]
+			tour[i] = tour[ind]
+			tour[ind] = temp
+
+	return tour
+
+
