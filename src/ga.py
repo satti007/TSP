01924d83b6e39e3eb21186e84f04383b48e5f4a7
Abinash Patra <abinashpatra@Abinashs-MacBook-Air.local>
2019-02-14 18:03:23 +0530

    Signature Added

--- src/ga.py
+++ src/ga.py
@@ -1,10 +1,31 @@
+from utils import *
 from sys import stdout
+import numpy as np
+
+def mutation(childPopulation, mutateProb):
+#returns children after mutation of dim popSize x numPoints
+
+def crossOver(selPopulation):
+#returns children after cross-over of dim popSize x numPoints
+
+def selection(population,numPoints,pointDist,popSize,eliteSize):
+#returns a selected population of dim popSize x numPoints
+
+def initPopulation(numPoints, popSize):
+#returns a random population of dim popSize x numPoints
 
 def geneticAlgo(numPoints,pointDist,popSize,eliteSize,mutateProb,generations):
+
+	pop = initPopulation(numPoints, popSize)
 	while True:
 		for gen in range(0,generations):
-			print gen
+			pop = selection(pop,numPoints,pointDist,popSize,eliteSize)
+			pop = crossOver(pop)
+			pop = mutation(pop, mutateProb)
 			stdout.flush()
 			if gen == 10:
 				break
 		break
+
+
+
---
+++ src/utils.py
@@ -0,0 +1,1 @@
+#define your helper functions
